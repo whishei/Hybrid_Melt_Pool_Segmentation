cec67b7c3c57739ea6f7fdb08baa37dd8b9de43b
whishei <sheila.whitman44@gmail.com>
2023-08-27 13:58:17 -0700

    Updates

---
+++ CNN_Model/IN718_CNN.py
@@ -0,0 +1,357 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+import torch
+import cv2
+import random
+import imageio
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pretrained_microscopy_models as pmm
+import segmentation_models_pytorch as smp
+import albumentations as albu
+
+from pathlib import Path
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as BaseDataset
+
+import time
+
+# set random seeds for repeatability
+random.seed(0)
+np.random.seed(0)
+torch.manual_seed(0)
+
+
+# model parameters
+architecture = 'UnetPlusPlus'
+encoder = 'resnet50' #resnext101_32x8d
+pretrained_weights = 'micronet' #'micronet' - giving error
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+print (device)
+
+# Create the Unet model with a resnet backbone that is pre-trained on micronet
+model = pmm.segmentation_training.create_segmentation_model(
+    architecture=architecture,
+    encoder = encoder,
+    encoder_weights=pretrained_weights, # use encoder pre-trained on micronet
+    classes=3 # secondary precipitates, tertiary precipitates, matrix
+    )
+    
+
+DATA_DIR = 'IN718-Training/' 
+
+x_train_dir = os.path.join(DATA_DIR, 'Train')
+y_train_dir = os.path.join(DATA_DIR, 'Train_annot')
+
+x_valid_dir = os.path.join(DATA_DIR, 'Val')
+y_valid_dir = os.path.join(DATA_DIR, 'Val_annot')
+'''
+#x_test_dir = os.path.join(DATA_DIR, 'test')
+#y_test_dir = os.path.join(DATA_DIR, 'test_annot')
+'''
+def get_training_augmentation():
+    train_transform = [
+        albu.Flip(p=0.75),
+        albu.RandomRotate90(p=1),       
+        albu.GaussNoise(p=0.5),
+        
+        albu.OneOf(
+            [
+                albu.CLAHE(p=1),
+                albu.RandomBrightness(p=1, limit=0.25),
+                albu.RandomGamma(p=1),
+            ],
+            p=0.50,
+        ),
+
+        albu.OneOf(
+            [
+                albu.Sharpen(p=1),
+                albu.Blur(blur_limit=3, p=1),
+                #albu.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.50,
+        ),
+
+        albu.OneOf(
+            [
+                albu.RandomContrast(p=1, limit=0.3),
+                albu.HueSaturationValue(p=1),
+            ],
+            p=0.50,
+        ),
+    ]
+    return albu.Compose(train_transform)
+
+def get_validation_augmentation():
+    """Add paddings to make image shape divisible by 32"""
+    # This is turned off for this dataset
+    test_transform = [
+        #albu.Resize(1000,1000)#height,width)
+    ]
+    return albu.Compose(test_transform)
+
+
+def to_tensor(x, **kwargs):
+    return x.transpose(2, 0, 1).astype('float32')
+
+print ("Check 1")
+def get_preprocessing(preprocessing_fn):
+    """Construct preprocessing transform
+    
+    Args:
+        preprocessing_fn (callbale): data normalization function 
+            (can be specific for each pretrained neural network)
+    Return:
+        transform: albumentations.Compose
+    
+    """
+    
+    _transform = [
+        albu.Lambda(image=preprocessing_fn),
+        albu.Lambda(image=to_tensor, mask=to_tensor),
+    ]
+    return albu.Compose(_transform)
+
+
+# how the images will be normalized. Use imagenet statistics even on micronet pre-training
+preprocessing_fn = smp.encoders.get_preprocessing_fn(encoder, 'imagenet') 
+
+# pixel values of the annotations for each mask.
+class_values = {'matrix': [0,0,255],
+               'secondary': [255,255,255],
+               'tertiary' : [255,0,0]
+}
+
+training_dataset = pmm.io.Dataset(
+    images=x_train_dir,
+    masks=y_train_dir,
+    class_values=class_values,
+    augmentation=get_training_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn)
+)
+validation_dataset = pmm.io.Dataset(
+    images=x_valid_dir,
+    masks=y_valid_dir,
+    class_values=class_values,
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn)
+)
+
+'''
+test_dataset = pmm.io.Dataset(
+    images=x_test_dir,
+    masks=y_test_dir,
+    class_values=class_values,
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn)
+)
+
+# validation data
+
+visualize_dataset = pmm.io.Dataset(
+    images=x_valid_dir,
+    masks=y_valid_dir,
+    class_values=class_values,
+    augmentation=get_validation_augmentation(),
+    #preprocessing=get_preprocessing(preprocessing_fn)
+)
+print ("Check 2")
+for im, mask in visualize_dataset:
+    pmm.util.visualize(
+        image=im,
+        matrix_mask=mask[...,0].squeeze(),
+        secondary_mask=mask[...,1].squeeze(),
+        tertiary=mask[...,2].squeeze(),
+    )
+'''
+# augmented training data
+
+augmented_dataset = pmm.io.Dataset(
+    images=x_train_dir,
+    masks=y_train_dir,
+    class_values=class_values,
+    augmentation=get_training_augmentation(),
+    #preprocessing=get_preprocessing(preprocessing_fn)
+)
+'''
+for im, mask in augmented_dataset:
+    pmm.util.visualize(
+        image=im,
+        matrix_mask=mask[...,0].squeeze(),
+        secondary_mask=mask[...,1].squeeze(),
+        tertiary=mask[...,2].squeeze(),
+    )
+    
+'''
+
+#Originally Training the model or finetuning. 
+'''
+#model_path = 'IN718_model.tar'
+state = pmm.segmentation_training.train_segmentation_model(
+    model=model,
+    architecture=architecture,
+    encoder=encoder,
+    train_dataset=training_dataset,
+    validation_dataset=validation_dataset,
+    class_values=class_values,
+    patience=30,
+    lr=1e-5,
+    batch_size=6,
+    val_batch_size=6,
+    device = device,
+    save_folder='models',
+    save_name='IN718_model.tar'
+)
+'''
+
+'''
+
+plt.plot(state['train_loss'], label='train_loss')
+plt.plot(state['valid_loss'], label='valid_loss')
+plt.legend()
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
+
+# drop the learning rate and keep training to see if we can squeeze a little more out.
+model_path = Path('models', 'UnetPlusPlus_resnet50_high_lr.pth.tar')
+state = pmm.segmentation_training.train_segmentation_model(
+    model=str(model_path),
+    architecture=architecture,
+    encoder=encoder,
+    patience=30,
+    lr=1e-5,
+    batch_size=6,
+    val_batch_size=6,
+    train_dataset=training_dataset,
+    validation_dataset=validation_dataset,
+    class_values=class_values,
+    save_folder='models',
+    save_name='UnetPlusPlus_resnet50_low_lr.pth.tar'
+)
+
+
+plt.plot(state['train_loss'], label='train_loss')
+plt.plot(state['valid_loss'], label='valid_loss')
+plt.legend()
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
+
+# load best model
+best_model_path = Path('models', 'model_best.pth.tar')
+state = torch.load(best_model_path) 
+print ("Check")
+best_model = pmm.segmentation_training.create_segmentation_model(
+    architecture=architecture,
+    encoder = encoder,
+    encoder_weights=pretrained_weights, # use encoder pre-trained on micronet
+    classes=3 # secondary precipitates, tertiary precipitates, matrix
+    )
+best_model.load_state_dict(pmm.util.remove_module_from_state_dict(state['state_dict']))
+print ("Check2")
+
+# create test dataset
+test_dataset = pmm.io.Dataset(
+    x_test_dir, 
+    y_test_dir, 
+    augmentation=get_validation_augmentation(), 
+    preprocessing=get_preprocessing(preprocessing_fn),
+    class_values=class_values,
+)
+
+test_dataloader = DataLoader(test_dataset)
+print ("Check 3")
+# test dataset without transformations for image visualization
+test_dataset_vis = pmm.io.Dataset(
+    x_test_dir, y_test_dir, 
+    class_values=class_values,
+)
+
+# evaluate model on test set
+loss = pmm.losses.DiceBCELoss(weight=0.7)
+metrics = [smp.utils.metrics.IoU(threshold=0.5),]
+test_epoch = smp.utils.train.ValidEpoch(
+    model=best_model,
+    loss=loss,
+    metrics=metrics,
+    device=device,
+)
+
+logs = test_epoch.run(test_dataloader)
+print ("Check 4")
+for n in range(len(test_dataset)):
+    
+    image_vis = test_dataset_vis[n][0].astype('uint8')
+    image, gt_mask = test_dataset[n]
+    
+    gt_mask_background = gt_mask[0].squeeze()
+    gt_mask_second = gt_mask[1].squeeze()
+    gt_mask_tert = gt_mask[2].squeeze()
+
+    x_tensor = torch.from_numpy(image).to(device).unsqueeze(0)
+    pr_mask = best_model.predict(x_tensor)
+
+
+    pr_mask = (pr_mask.squeeze().cpu().numpy().round())
+    pr_mask_second = pr_mask[1].squeeze()
+    pr_mask_tert = pr_mask[2].squeeze()
+    
+    pr_mask_background = pr_mask[0].squeeze()
+        
+    pmm.util.visualize(
+        image=image_vis, 
+        ground_truth_secondary=gt_mask_second,
+        predicted_secondary=pr_mask_second,
+        ground_truth_tertiary=gt_mask_tert,
+        predicted_tertiary=pr_mask_tert
+    )
+'''
+
+model_path = Path('models', 'IN718.pth.tar')
+model, preprocessing_fn = pmm.segmentation_training.load_segmentation_model(model_path, classes=3)
+
+#print ("Check 5")
+
+#Image.MAX_IMAGE_PIXELS = 399279028
+
+im_path = 'Cropped.tif'
+im = imageio.imread(im_path)
+#truth = imageio.imread(annot_path)
+#truth = truth[:,:,:3]
+st = time.time()
+pred = pmm.segmentation_training.segmentation_models_inference(im, model, preprocessing_fn, batch_size=4, patch_size=512,
+                                                     probabilities=None)
+end = time.time()
+execution = end - st
+print (execution)
+#print (pred.shape)
+#meltpools = pred[...,0]
+#background = pred[...,1]
+
+np.save('cropped_melts.npy',pred)
+#np.save('Back.npy', background)
+
+'''
+fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(16,16))
+ax0.imshow(im)
+ax0.set_title('Original Image')
+ax1.imshow(pred[...,0])
+ax1.set_title('Tertiary precipitates')
+ax2.imshow(pred[...,1])
+ax2.set_title('Tertiary precipitates')
+plt.savefig('crop45_seg_new2.png')
+'''
+'''
+labels = [[0,0,255], [255,0,0]]
+visual = pmm.segmentation_training.visualize_prediction_accuracy(pred, truth, labels)
+plt.figure(figsize=(12,12))
+plt.imshow(visual)
+plt.savefig('New.png')
+'''
---
+++ Random_Forest_Model/IN718_CNN.py
@@ -0,0 +1,187 @@
+#UTEP Project
+import numpy as np
+import skimage
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+from PIL import Image
+from scipy import ndimage as ndi
+from sklearn.ensemble import RandomForestClassifier
+from functools import partial
+
+from skimage.feature import canny, blob_dog, peak_local_max
+from skimage.util import invert
+from skimage import data
+from skimage import color
+from skimage.filters import meijering, sato, frangi, hessian
+from skimage.morphology import skeletonize, thin, dilation, disk,closing
+from skimage.segmentation import random_walker
+from skimage.filters.rank import median 
+from skimage import segmentation, feature, future
+import time
+
+
+#Opening the training data and the test data 
+
+Image.MAX_IMAGE_PIXELS = 399279028
+my_dpi = 3600
+
+#Opening the first training image
+train1_raw = Image.open('IN718-Training/Cropped_1.tif')
+train1 = train1_raw.convert('L')
+train1 = np.asarray(train1)
+
+#Opening the second training image
+train2_raw = Image.open('IN718-Training/Cropped_2.tif')
+train2 = train2_raw.convert('L')
+train2 = np.asarray(train2)
+
+#Opening the third training image
+train3_raw = Image.open('IN718-Training/Cropped_3.tif')
+train3 = train3_raw.convert('L')
+train3 = np.asarray(train3)
+
+'''
+#Visualizing training images
+plt.imshow(train1,cmap=plt.cm.gray)
+plt.show()
+plt.imshow(train2,cmap=plt.cm.gray)
+plt.show()
+plt.imshow(train3,cmap=plt.cm.gray)
+plt.show()
+'''
+#Opening the first training mask
+train1_mask = np.load('IN718-Training/Cropped_1_mask.npy')
+
+#Opening the second training mask
+train2_mask = np.load('IN718-Training/Cropped_2_mask.npy')
+
+#Opening the third training mask
+train3_mask = np.load('IN718-Training/Cropped_3_mask.npy')
+
+'''
+#Visualizing training masks
+plt.imshow(train1_mask,cmap=plt.cm.gray)
+plt.colorbar()
+plt.show()
+plt.imshow(train2_mask,cmap=plt.cm.gray)
+plt.colorbar()
+plt.show()
+plt.imshow(train3_mask,cmap=plt.cm.gray)
+plt.colorbar()
+plt.show()
+'''
+
+
+#Opening the test image
+test_image_raw = Image.open('Testing/crop45.tif')
+test_image = test_image_raw.convert('L')
+test_image = np.asarray(test_image)
+
+'''
+#Visualizing test image
+plt.imshow(test_image,cmap=plt.cm.gray)
+plt.colorbar()
+plt.show()
+'''
+
+print ("Check1")
+
+#############Random Forest Classifier
+
+sigma_min = 1
+sigma_max = 16
+features_func = partial(feature.multiscale_basic_features,
+                        intensity=True, edges=False, texture=True,
+                        sigma_min=sigma_min, sigma_max=sigma_max)
+
+#Features for the two training images
+train1_features = features_func(train1)
+train2_features = features_func(train2)
+train3_features = features_func(train3)
+
+
+clf = RandomForestClassifier(n_estimators=50, n_jobs=-1,
+                             max_depth=10, max_samples=0.05)
+
+#Reshaping from mXm array to (mxm)x1 array
+train1_mask = train1_mask.reshape(-1)
+train2_mask = train2_mask.reshape(-1)
+train3_mask = train3_mask.reshape(-1)
+
+#Reshaping from mxmx15 to (mxm)x15 array
+train1_features_ = np.reshape(train1_features, (-1,train1_features.shape[2]))
+train2_features_ = np.reshape(train2_features, (-1,train2_features.shape[2]))
+train3_features_ = np.reshape(train3_features, (-1,train3_features.shape[2]))
+
+#Choosing only the known and labeled pixels
+ind1 = (train1_mask > -1)
+ind2 = (train2_mask > -1)
+ind3 = (train3_mask > -1)
+
+#Choosing only the unknown and unlabeled pixels
+ind1_ = (train1_mask == -1)
+ind2_ = (train2_mask == -1)
+ind3_ = (train3_mask == -1)
+
+#Creating the training data 
+X_train1 = train1_features_[ind1,:]
+X_train2 = train2_features_[ind2,:]
+X_train3 = train3_features_[ind3,:]
+y_train1 = train1_mask[ind1]
+y_train2 = train2_mask[ind2]
+y_train3 = train3_mask[ind3]
+
+X_train = np.append(X_train1,X_train2,axis = 0)
+X_train = np.append(X_train,X_train3,axis = 0)
+
+y_train = np.append(y_train1,y_train2)
+y_train = np.append(y_train,y_train3)
+
+print ("Check2")
+clf.fit(X_train,y_train)
+print ("Check3")
+
+#Predicting for our test image
+test_features = features_func(test_image)
+TEST_image = np.reshape(test_features, (-1,test_features.shape[2]))
+X_test = TEST_image 
+
+#Timing
+st = time.time()
+y_test = clf.predict(X_test)
+end = time.time()
+execution = end - st
+print (execution)
+print ("Check4")
+
+
+#Reshaping back into orginal image shape
+remake_img = np.reshape(y_test, test_image.shape)
+
+#Unique classifications (0 - boundary, 1 - background, 2 - interior)
+values = np.unique(remake_img)
+
+
+#Visualization
+fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(9, 4))
+ax[0].imshow(test_image,cmap=plt.cm.gray)
+ax[0].set_title('Raw Image')
+ax[1].imshow(remake_img, cmap=plt.cm.gray)
+ax[1].set_title('Segmentation of crop45.tif')
+fig.tight_layout()
+#plt.savefig('Results\crop45_seg1.png', bbox_inches='tight', pad_inches=0)
+plt.show()
+
+
+#Saving segmentation matrix
+#np.save('crop45_melts.npy',remake_img)
+
+'''
+#Visualization_specific
+plt.figure(dpi = my_dpi)
+plt.imshow(remake_img,cmap=plt.cm.gray)
+plt.axis('off')
+#plt.savefig('Results\crop45_seg2.png', bbox_inches='tight', pad_inches=0)
+plt.show()
+'''
